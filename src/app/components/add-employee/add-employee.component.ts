544565bb8a62f7abb631544c102dc46b36cb3e2c
unknown <khaledboussaba@gmail.com>
2019-12-08 23:35:05 +0100

    Add Employee WORKS !!!

--- src/app/components/add-employee/add-employee.component.ts
+++ src/app/components/add-employee/add-employee.component.ts
@@ -5,6 +5,8 @@ import { Employee } from '../../Employee';
 import { FlashMessagesService } from 'angular2-flash-messages';
 import { Router } from '@angular/router';
 
+import { EmployeeService } from '../../services/employee.service';
+
 @Component({
   selector: 'app-add-employee',
   templateUrl: './add-employee.component.html',
@@ -24,18 +26,26 @@ export class AddEmployeeComponent implements OnInit {
 
   disableSalary: boolean = true;
 
-  constructor(public flashMessagesService: FlashMessagesService, public router: Router) { }
+  constructor(public flashMessagesService: FlashMessagesService, public router: Router, public employeeService: EmployeeService) { }
 
   ngOnInit() {
   }
 
   addEmployeeSubmit({ value, valid }: { value: Employee, valid: boolean }) {
+
+    if(this.disableSalary) {
+      value.salary = 0;
+    }
+
     if (!valid) {
       //console.log("not correct data");
       this.flashMessagesService.show('Please write correct informations for new employee', {cssClass:'alert-danger', timeout: 6000});
       this.router.navigate(['add-employee']);
     } else {
-      console.log(this.employee);
+      //console.log(this.employee);
+      this.employeeService.addEmployee(value);
+      this.flashMessagesService.show('New employee added successfully', {cssClass:'alert-success', timeout: 6000});
+      this.router.navigate(['/']);
     }
   }
 
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase, AngularFireList , AngularFireObject } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from 'angularfire2/database';
 import { Observable } from 'rxjs';
 import { Employee } from '../Employee';
 
@@ -8,15 +8,19 @@ import { Employee } from '../Employee';
 })
 export class EmployeeService {
 
-  employees : AngularFireList<Employee[]>;
-  Employee : AngularFireObject<any>;
+  employees: AngularFireList<any>;
+  Employee: AngularFireObject<any>;
 
-  constructor(private angularfire:AngularFireDatabase) {
+  constructor(private angularfire: AngularFireDatabase) {
     this.employees = this.angularfire.list('/employees/employees') as AngularFireList<Employee[]>;
-   }
+  }
 
-   getEmployees() {
-     return this.employees;
-   }
+  getEmployees() {
+    return this.employees;
+  }
+
+  addEmployee(employee:Employee) {
+    return this.employees.push(employee);
+  }
 
 }
